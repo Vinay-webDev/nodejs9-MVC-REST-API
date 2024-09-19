8013fc332b4b2df2881c9d54d4ca9cfd4875bb3b
Vinay-webDev <justchatting3000@gmail.com>
2024-09-19 21:41:10 +0530

    2. config
    corsOptions in config folder
    and deleted some directories and files for the clean up✅

---
+++ config/corsOptions.js
@@ -0,0 +1,15 @@
+// Let's clean up this👇
+const whitelist = ['https://www.yoursite.com', 'http://127.0.0.1:5500', 'http://localhost:3500'];
+
+const corsOptions = {
+    origin: (origin, callback) => {
+        if ( whitelist.indexOf(origin) !== -1 || !origin ) {
+            callback(null, true); 
+        } else {
+            callback(new Error('not allowed by the CORS'));
+        }
+    },
+    optionsSuccessStatus: 200 
+}
+
+module.exports = corsOptions;
--- server.js
+++ server.js
@@ -4,23 +4,10 @@ const path = require('path');
 const { logger } = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
 const cors = require('cors');
-
+const corsOptions = require('./config/corsOptions');
 const PORT = process.env.PORT || 3500;
 
 app.use(logger);
-
-const whitelist = ['https://www.yoursite.com', 'http://127.0.0.1:5500', 'http://localhost:3500'];
-
-const corsOptions = {
-    origin: (origin, callback) => {
-        if ( whitelist.indexOf(origin) !== -1 || !origin ) {
-            callback(null, true); 
-        } else {
-            callback(new Error('not allowed by the CORS'));
-        }
-    },
-    optionsSuccessStatus: 200 
-}
 
 app.use(cors(corsOptions));
 
@@ -30,7 +17,6 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/subdir', require('./routes/subdir'));
 
 app.use('/', require('./routes/root'));
 
